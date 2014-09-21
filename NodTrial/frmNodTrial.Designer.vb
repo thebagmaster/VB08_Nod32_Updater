f4bfa1dba156ed8df9d88ddbef5276ecdb60e3a7
Daniel Nichols <thebagmaster@gmail.com>
2014-09-20 23:12:09 -0500

    First Commit

---
+++ NodTrial/frmNodTrial.Designer.vb
@@ -0,0 +1,91 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNodTrial
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmNodTrial))
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NodTrial = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.AllowNavigation = False
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(292, 164)
+        Me.WebBrowser1.TabIndex = 1
+        Me.WebBrowser1.WebBrowserShortcutsEnabled = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(26, 0)
+        Me.TextBox1.MaxLength = 99999999
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(10, 10)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.WordWrap = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'NodTrial
+        '
+        Me.NodTrial.Icon = CType(resources.GetObject("NodTrial.Icon"), System.Drawing.Icon)
+        Me.NodTrial.Text = "NodTrial"
+        Me.NodTrial.Visible = True
+        '
+        'frmNodTrial
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 164)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmNodTrial"
+        Me.ShowInTaskbar = False
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "NodTrial"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents NodTrial As System.Windows.Forms.NotifyIcon
+
+End Class
---
+++ NodTrial/frmNodTrial.vb
@@ -0,0 +1,85 @@
+﻿Public Class frmNodTrial
+    Const KEYEVENTF_KEYUP = &H2
+    Dim tmps As String
+    Dim usr(500) As String
+    Dim pas(500) As String
+    Dim i As Integer
+    Dim pos As Integer
+    Dim r As Integer
+    Private Declare Sub keybd_event Lib "user32.dll" (ByVal bVk As Byte, ByVal bScan As Byte, ByVal dwFlags As Long, ByVal dwExtraInfo As Long)
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Randomize()
+        WebBrowser1.Navigate("http://www.nod32keys.com/")
+        Timer1.Enabled = True
+    End Sub
+    Public Function RemoveTag(ByVal InpData As String) As String
+        Dim intStart, intEnd As Integer
+        Dim strExtract, strInput As String
+        strInput = InpData
+        While Instr(strInput, "<") <> 0
+            intStart = Instr(strInput, "<")
+            intEnd = Instr(strInput, ">")
+            If intStart > 1 Then
+                strExtract = Mid(strInput, 1, intStart - 1) & Mid(strInput, intEnd + 1, Len(strInput) - intEnd)
+            Else
+                strExtract = Mid(strInput, intEnd + 1, Len(strInput) - intEnd)
+            End If
+            strInput = strExtract
+        End While
+        Return strInput
+    End Function
+    Private Function getkeys()
+        tmps = TextBox1.Text
+        tmps = tmps.ToLower
+        tmps = tmps.Replace(" ", "")
+        TextBox1.Text = "u" & RemoveTag(tmps.Substring(InStr(tmps, "username:"), InStr(InStr(tmps, "username:"), tmps, vbCrLf) - InStr(tmps, "username:")) & "username:")
+        i = 0
+        pos = 1
+        While Not pos = 0
+            If InStr(pos, TextBox1.Text, "password:") = 0 Then
+                Exit While
+            End If
+            usr(i) = TextBox1.Text.Substring(InStr(pos, TextBox1.Text, "username:") + 8, (InStr(pos, TextBox1.Text, "password:") - (InStr(pos, TextBox1.Text, "username:") + 8)) - 1)
+            pos = InStr(pos, TextBox1.Text, "password:")
+            pas(i) = TextBox1.Text.Substring(InStr(pos, TextBox1.Text, "password:") + 8, (InStr(pos, TextBox1.Text, "username:") - (InStr(pos, TextBox1.Text, "password:") + 8)) - 1)
+            pos = InStr(pos, TextBox1.Text, "username:")
+            i = i + 1
+        End While
+        r = Int((i + 1) * Rnd())
+        Return 1
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If Not WebBrowser1.Document.Window.Frames.Count = 7 Then
+            Exit Sub
+        Else
+            TextBox1.Text = WebBrowser1.Document.Body.OuterHtml
+            Timer1.Enabled = False
+            getkeys()
+            Dim pList() As System.Diagnostics.Process = _
+                System.Diagnostics.Process.GetProcesses
+            For Each proc As System.Diagnostics.Process In pList
+                If proc.ProcessName = "egui" Then
+                    Shell("C:\Program Files\ESET\ESET Smart Security\egui.exe")
+                    Dim a As Integer
+                    Do Until a = 100000
+                        a = a + 1
+                        Application.DoEvents()
+                    Loop
+                    SendKeys.SendWait("^u")
+                    SendKeys.SendWait(usr(r))
+                    SendKeys.SendWait("{TAB}")
+                    SendKeys.SendWait(pas(r))
+                    SendKeys.SendWait("{ENTER}")
+                    SendKeys.SendWait("%{F4}")
+                    Do Until a = 100000
+                        a = a + 1
+                        Application.DoEvents()
+                    Loop
+                    NodTrial.Visible = False
+                    Me.Dispose()
+                End If
+            Next
+        End If
+    End Sub
+End Class
